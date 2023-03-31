69858d60362e3e5372ec7e15386e85209e2fa27e
Jesuslovesshiva <111922763+Jesuslovesshiva@users.noreply.github.com>
2023-03-31 20:36:11 +0200

    continued eventcard

---
+++ app/components/pages/TrainingPlan/icons.jsx
@@ -0,0 +1,25 @@
+import {
+  faLocationDot,
+  faUser,
+  faImage,
+  faCirclePlay,
+  faFileLines,
+  faVideo,
+  faClock,
+  faPersonArrowUpFromLine,
+  faLocationCrosshairs
+} from '@fortawesome/free-solid-svg-icons'
+
+export const icons = {
+  locationDot: faLocationDot,
+  user: faUser,
+  image: faImage,
+  circlePlay: faCirclePlay,
+  fileLines: faFileLines,
+  video: faVideo,
+  clock: faClock,
+  personArrowUpFromLine: faPersonArrowUpFromLine,
+  target: faLocationCrosshairs
+}
+
+export default icons
